53d6b73069e34622c2a9c1d7edb84836dce8b237
sabinuki <blue.apple.pie63@gmail.com>
2021-09-20 15:32:43 +0900

    rubocopで警告されていたマイグレーションファイル修正

--- backend/db/migrate/20210918121356_add_user_id_to_users.rb
+++ backend/db/migrate/20210918121356_add_user_id_to_users.rb
@@ -1,5 +1,9 @@
 class AddUserIdToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :user_id, :string, null: false, after: :email
+    change_table :users, bulk: true do |t|
+      t.string :user_id, null: false, default: '', after: :email
+
+      t.index :user_id, unique: true
+    end
   end
 end
