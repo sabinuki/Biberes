30e419187e800c085ed4c144d00c33e1b72ea4da
sabinuki <blue.apple.pie63@gmail.com>
2022-01-01 20:29:41 +0900

    ビアスタイルマスタ作成

---
+++ backend/app/importers/beer_style_importer.rb
@@ -0,0 +1,55 @@
+class BeerStyleImporter
+  require 'csv'
+
+  def self.execute
+    new.execute
+  end
+
+  def execute
+    import_style_class
+    import_style
+  end
+
+  private
+
+  def import_style_class
+    saved_names = BeerStyleClass.pluck(:name)
+    file_path = Rails.root.join('db/seeds/beer_style/style_class.csv')
+    csv = CSV.read(file_path, headers: true)
+
+    style_classes = csv.map do |row|
+      next if saved_names.include?(row['name'])
+
+      {
+        name: row['name']
+      }
+    end
+
+    style_classes.compact!
+
+    BeerStyleClass.import(style_classes)
+  end
+
+  def import_style
+    saved_names = BeerStyle.pluck(:name)
+    saved_classes = BeerStyleClass.pluck(:id, :name).to_h
+
+    return if saved_classes.blank?
+
+    file_path = Rails.root.join('db/seeds/beer_style/style.csv')
+
+    csv = CSV.read(file_path, headers: true)
+    style = csv.map do |row|
+      next if saved_names.include?(row['name'])
+
+      {
+        name: row['name'],
+        beer_style_id: saved_classes.key(row['class'])
+      }
+    end
+
+    style.compact!
+
+    BeerStyle.import(style)
+  end
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_121356) do
+ActiveRecord::Schema.define(version: 2021_12_31_094905) do
+
+  create_table "beer_style_classes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "beer_styles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "beer_style_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
