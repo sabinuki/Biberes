525ce37f89f3be4760eaa7e95ed4d9cf5b846e38
sabinuki <blue.apple.pie63@gmail.com>
2022-09-25 21:53:22 +0900

    Brewery#nameのvalidateにlengthを追加

--- backend/app/models/brewery.rb
+++ backend/app/models/brewery.rb
@@ -1,5 +1,5 @@
 class Brewery < ApplicationRecord
   has_many :beers, dependent: :destroy
 
-  validates :name, uniqueness: true, presence: true
+  validates :name, uniqueness: true, presence: true, length: { maximum: 255 }
 end
