48b99fc7ed64f269cb46730c916cbf0e33fc81fb
sabinuki <blue.apple.pie63@gmail.com>
2022-09-27 20:54:34 +0900

    RQコード生成ロジック作成

---
+++ frontend/src/components/Elements/QRCode.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useQRCode } from 'react-qrcodes';
+
+export default function BreweryQRCode() {
+  const [inputRef] = useQRCode({
+    // TODO: textに遷移させたいURLを記述する
+    text: 'entrypoint/breweries/3',
+    options: {
+      level: 'H', //誤り訂正レベル
+      margin: 3, //QRコードの周りの空白マージン
+      scale: 1,
+      width: 200,
+    },
+  });
+
+  return <canvas ref={inputRef} />;
+};
