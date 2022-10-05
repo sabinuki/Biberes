8a510e3b1dea08b0057c8a3c7d7e84ab59f89e7f
sabinuki <blue.apple.pie63@gmail.com>
2022-10-05 20:25:58 +0900

    mock用にbeer the firstのページを作成

--- frontend/src/containers/Brewery.jsx
+++ frontend/src/containers/Brewery.jsx
@@ -1,33 +1,38 @@
-import { Box, Container, List, ListItem, ListItemText, Typography } from '@mui/material';
+import { Box, Card, CardContent, CardMedia, Container, Grid, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { getBrewery } from 'lib/api/breweries';
+import beer0 from 'static/images/tmp/beer0.JPG';
+import beer1 from 'static/images/tmp/beer1.JPG';
+import beer2 from 'static/images/tmp/beer2.JPG';
+import beer3 from 'static/images/tmp/beer3.JPG';
+
+const cards = [beer0, beer1, beer2, beer3];
 
 export const Brewery = ({ match }) => {
   const [brewery, setBrewery] = useState('');
+  // TODO: ビールの登録機能を作成したらbeersをUI表示する
   const [beers, setBeers] = useState([]);
   const params = { id: match.params.id }
 
   const Beers = () => {
     return (
-      <List>
-        {beers.map((beer, i) => {
-          return (
-            <ListItem key={i}>
-              <ListItemText>{beer.name}</ListItemText>
-            </ListItem>
-          );
-        })}
-      </List>
-    );
-  }
-
-  const GettingReady = () => {
-    return (
-      <Container component="main" maxWidth="xs">
-        <Typography>
-          ビール情報準備中です
-        </Typography>
-      </Container>
+      <Grid container spacing={4}>
+        {cards.map((card, i) => (
+          <Grid item key={i} xs={12} sm={6} md={4}>
+            <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+              <CardMedia component="img" image={card} alt="beer" />
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Beer
+                </Typography>
+                <Typography>
+                  Style: Wheat Ale
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     );
   }
 
@@ -49,11 +54,14 @@ export const Brewery = ({ match }) => {
 
   return (
     <Container maxWidth="md" sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Typography variant="h2">
+      <Typography gutterBottom variant="h2">
         {brewery.name}
       </Typography>
+      <Typography variant="h5" color="text.secondary">
+        Beers
+      </Typography>
       <Box sx={{ marginTop: 8 }}>
-        {beers.length > 0 ? <Beers /> : <GettingReady />}
+        <Beers />
       </Box>
     </Container>
   );
